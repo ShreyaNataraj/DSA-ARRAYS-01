9bf92bb85ea9b69ec51484fa07a3afdd7854dde3
Shreya N <nshreya094@gmail.com>
2024-01-17 00:35:25 +0530

    subarrayssum problems

--- subArrays.java
+++ subArrays.java
@@ -1,20 +1,59 @@
 public class subArrays {
-  public static void subarrays(int arr[]){
-    for(int i = 0; i<arr.length; i++){
+  public static void printsubarrays(int numbers[]){
+    int currSum = 0;
+    int maxSum= Integer.MIN_VALUE;
+    for(int i = 0; i<numbers.length; i++){
       int start = i;
-      for(int j = i; j<arr.length; j++){
+      for(int j = i; j<numbers.length ; j++){
         int end = j;
-        for(int k =start; k<=end; k++){
-        System.out.print( arr[k] + " ");
-      } 
-      System.out.println();
+        currSum=0;
+        for(int k = start; k<=end; k++){
+          currSum+=numbers[k];}
+          System.out.print(currSum + " ");
+          if(currSum>maxSum){
+            maxSum =currSum;
+          }
+        }
+        
+      }
+      System.out.println("maximum Sum is " + maxSum + " ");
     }
-    System.out.println();
+
+  public static void maxSubarray(int numbers[]){
+    int currsum = 0;
+    int maxsum = Integer.MIN_VALUE;
+    int prefix[] = new int[numbers.length];
+    prefix[0] = numbers[0];
+    for(int i = 1; i<numbers.length ; i++){
+      prefix[i] = prefix[i-1]+numbers[i];
+    }
+    for(int i =0; i<numbers.length ; i++){
+      for(int j =0;j<numbers.length; j++ ){
+        currsum =0;
+        currsum =(i==0)?prefix[j]:prefix[j]-prefix[i-1];
+        if(currsum>maxsum){
+          maxsum =currsum;
+        }
+      }
+    }
+    System.out.print(maxsum);
   }
-System.out.println(ts);
+public static void kadens(int numbers[]){
+  int currsum = 0;
+  int maxsum =Integer.MIN_VALUE;
+ for(int i =0; i<numbers.length; i++){
+  currsum = currsum +numbers[i];
+  if(currsum<0){
+    currsum =0;
   }
-public static void main(String args[]){
-  int arr[] = {1,2,3,4,5,6};
-  subarrays(arr);
-}
+  maxsum =Math.max(currsum,maxsum);
+ }
+ System.out.print(maxsum);
+  }
+
+  
+  public static void main(String args[]){
+    int numbers[] = {1,2,3,4,5,6,7};
+    kadens(numbers);
+  }
 }
